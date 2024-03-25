28ed6cefaa09e7ee021abff5817f2f2ebfba1b24
Alexandra Camarena <alexandracamarena@ou.edu>
2024-03-25 15:54:17 -0500

    Add project files.

---
+++ Exam2_AlexandraCamarena/Program.cs
@@ -0,0 +1,9 @@
+﻿// MIS 3013 001
+// Exam 2
+// March 27, 2024
+// Alexandra Camarena
+//113568154
+
+Console.WriteLine("Exam 2");
+Console.WriteLine("Your real name here");
+Console.WriteLine("[Exam 2 code]");// You are going to get the exam 2 code during the exam.
